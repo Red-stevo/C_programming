49af60cc0a0c7b0a885b35d4ebeece46633a7aa0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-29 06:25:09 +0300

    Commit message >>>>> configured the switch case. <<<<<

--- pointers.c
+++ pointers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct{
   char name[30];
@@ -10,11 +11,39 @@ typedef struct{
 
 
 int main(int argc, char *argv[]){
-  
-  printf("%d", argc);
-  
-  for(int i= 0 ; i < argc ; i++){
-    printf("%s\n", argv[i]);
+  char choice;
+
+
+  while(true){
+    printf("\tWhat would you like to do?\n");
+    printf("1. Add student details.\n");
+    printf("2. View a Student's details.\n");
+    printf("3. View Students Details.\n");
+    printf("4. Delete a Student's Details.\n");
+    printf("5. Exit.\n");
+
+    printf("Choice : ");
+    scanf(" %c", &choice);
+    switch(choice){
+      case '1':
+        //add students.
+        break;
+      case '2':
+        //view one student's details.
+        break;
+      case '3':
+        //view all student's details.
+        break;
+      case '4':
+        //delete a students details.
+        break;
+      case '5':
+        exit(0);
+      default:
+        printf("Invalid Choice try Again.\n");
+    }
   }
+
+
   return 0;
 }
