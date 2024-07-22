47bffcee4b02a71022b48908cfa3185672e60bbd
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-22 09:44:57 +0300

    Commit message >>>>> all done for redirection of streams. <<<<<

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main(void)
-{
+int main(void){
    int i, sum = 0;
+   puts(__TIME__);
    while (true){
       scanf("%d", &i);
       if(i == 0){
-         printf("Sum : %d", sum);
+         printf("Sum : %d\n", sum);
+         puts(__TIME__);
          return sum;
       }
       sum = sum + i;
