81d5465f81e96e300c54d2f06dea9593f04b4df2
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-08 17:34:44 +0300

    Commit message >>>>> workong on system programming <<<<<

--- exam-questions/main.c
+++ exam-questions/main.c
@@ -2,8 +2,18 @@
 #include <unistd.h>
 
 
-int main(void)
-{
+void working_with_fork(){
+
+    fork();
+    int return_value = fork();
+
+    if (return_value == 0){
+        printf("Child process is : %d\n", getpid());
+        printf("return value : %d\n", return_value);
+    }else{
+        printf("Parent process id %d\n", getpid());
+        printf("return value : %d\n", return_value);
+    }
 
     fork();
     fork();
@@ -13,12 +23,21 @@ int main(void)
         printf("Parent id %d \n",getppid());
 
         printf("Hello From The Child Process.\n");
-    }
-    else{
+    }else{
         sleep(5);
         printf("process id : %d \n", getpid());
         printf("\tHello, World!\n");
-
     }
+}
+
+
+
+
+
+int main(void){
+
+
+
+
     return 0;
 }
